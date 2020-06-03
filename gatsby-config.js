f128fc30c3e387f89c3b0d947667e9cae326910a
Raj Rathore <email@rajrathore.com>
2020-06-03 16:33:12 +0530

    feat(analytics): google analytics added

--- gatsby-config.js
+++ gatsby-config.js
@@ -78,5 +78,18 @@ module.exports = {
                 ],
             },
         },
+        {
+            resolve: `gatsby-plugin-google-analytics`,
+            options: {
+                // replace "UA-XXXXXXXXX-X" with your own Tracking ID
+                trackingId: `UA-50333833-1`,
+            },
+        },
+        {
+            resolve: `gatsby-plugin-disqus`,
+            options: {
+                shortname: `rajrathoreblog`,
+            },
+        },
     ],
 }
