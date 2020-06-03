03700eeb3c6fd6ff330249bd56c8c1e5023dc689
Raj Rathore <email@rajrathore.com>
2020-06-03 14:48:38 +0530

    fix(meta): fixed meta data, path and icons

--- siteConfig.js
+++ siteConfig.js
@@ -1,23 +1,23 @@
 module.exports = {
     // Do not include a trailing slash!
-    siteUrl: `https://demo.jamify.org`,
+    siteUrl: `https://www.rajrathore.com`,
 
     // Maximum number of post shown per page
     // With Infinite Scroll this is the chunk of posts fetched when scrolling down
     postsPerPage: 3,
 
     // This allows an alternative site title for meta data for pages.
-    siteTitleMeta: `Gatsby Starter Ghost CMS`,
+    siteTitleMeta: `Raj Rathore&#x27;s Blog`,
 
     // This allows an site description for meta data for pages.
-    siteDescriptionMeta: `Turn your Ghost blog into a flaring fast static site with Gatsby`,
+    siteDescriptionMeta: `A sneak peek into my life.... a programmer&#x27;s thoughts, stories and ideas.`,
 
     // Change to the width and height of your default share image
     shareImageWidth: 1000,
     shareImageHeight: 523,
 
     // Used for App and Offline manifest e.g. Mobile Home Screen
-    shortTitle: `Jamify`,
+    shortTitle: `Raj's Blog`,
     siteIcon: `favicon.png`,
     backgroundColor: `#e9e9e9`,
     themeColor: `#15171A`,
